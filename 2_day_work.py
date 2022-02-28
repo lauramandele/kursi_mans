cf5b7854c5074c203f118b38f145f75a1073cb78
Laura Mandele <laura.mandele@tietoevry.com>
2022-02-28 11:33:22 +0200

    fixure

--- 2_day_work.py
+++ 2_day_work.py
@@ -9,6 +9,7 @@ year_now = datetime.datetime.now().year
 print(f"It's going to be in year {year_now+(100-age)}")
 
 # Exercise Room volume
+print("Let's calculate room volume! ")
 width = input("Enter width:")
 length = input("Enter length:")
 height = input("Enter height:")
@@ -16,6 +17,7 @@ volume = float(width) * float(length) * float(height)
 print(f"Room volume is {volume}")
 
 # Exercise Temperature Conversion
+print("Temperature")
 temp_celsius = input("Enter temperature in Celsius:")
 temp_fahrenheit = 32 + (float(temp_celsius) * (9 / 5))
-print(f"The temperature in fahrenheit is {temp_fahrenheit}")
+print(f"The temperature in fahrenheit is {round(temp_fahrenheit, 2)}")
